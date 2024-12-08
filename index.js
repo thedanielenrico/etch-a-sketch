abeefe219b98a964e877ab5eefea9f8ac8fbd4d4
Daniel Enrico <thedanielenrico@gmail.com>
2024-12-08 14:31:39 -0800

    fix row box sizing issue

--- index.js
+++ index.js
@@ -4,28 +4,42 @@ const DEFAULT_GRID_SIZE = 16;
 const rowBoxes = [];
 const gridContainer = document.getElementById("grid-container");
 const clearBtn = document.getElementById("clear-btn");
+const rowAmountSlider = document.getElementById("box-range-slider");
+const sliderLabel = document.getElementById("slider-label");
+
+rowAmountSlider.addEventListener("input", (e) => {
+  gridContainer.innerHTML = "";
+  sliderLabel.innerText = e.target.value;
+  createGrid(Number(e.target.value));
+});
 
 clearBtn.addEventListener("click", () => {
   rowBoxes.forEach((box) => {
     box.style.backgroundColor = "initial";
+    box.style.borderColor = "lightblue";
   });
 });
 
-Array(DEFAULT_GRID_SIZE)
-  .fill(undefined)
-  .map(() => {
-    const row = document.createElement("div");
-    row.className = "row";
-    gridContainer.appendChild(row);
-    Array(DEFAULT_GRID_SIZE)
-      .fill(undefined)
-      .map((val, idx) => {
-        const rowBox = document.createElement("div");
-        rowBox.className = "rowBox";
-        rowBox.addEventListener("mouseenter", () => {
-          rowBox.style.backgroundColor = "blue";
+function createGrid(gridDimension) {
+  Array(gridDimension)
+    .fill(undefined)
+    .map(() => {
+      const row = document.createElement("div");
+      row.className = "row";
+      gridContainer.appendChild(row);
+      Array(gridDimension)
+        .fill(undefined)
+        .map((val, idx) => {
+          const rowBox = document.createElement("div");
+          rowBox.className = "rowBox";
+          rowBox.addEventListener("mouseenter", () => {
+            rowBox.style.backgroundColor = "blue";
+            rowBox.style.borderColor = "blue";
+          });
+          rowBoxes.push(rowBox);
+          row.appendChild(rowBox);
         });
-        rowBoxes.push(rowBox);
-        row.appendChild(rowBox);
-      });
-  });
+    });
+}
+
+createGrid(DEFAULT_GRID_SIZE);
