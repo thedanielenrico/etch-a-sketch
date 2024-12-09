9ac39d9522e66b45bdc736a0af7b2a3531363765
Daniel Enrico <thedanielenrico@gmail.com>
2024-12-09 14:02:01 -0800

    add darkening effect to squares

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ clearBtn.addEventListener("click", () => {
   rowBoxes.forEach((box) => {
     box.style.backgroundColor = "initial";
     box.style.borderColor = "lightblue";
+    box.style.opacity = 1;
   });
 });
 
@@ -30,12 +31,20 @@ function createGrid(gridDimension) {
       Array(gridDimension)
         .fill(undefined)
         .map(() => {
+          let brightness = 1.1;
           const rowBox = document.createElement("div");
           rowBox.className = "rowBox";
           rowBox.addEventListener("mouseenter", (e) => {
             if (!e.metaKey) {
               rowBox.style.backgroundColor = "blue";
               rowBox.style.borderColor = "blue";
+              if (brightness > 0) {
+                rowBox.style.filter = `brightness(${
+                  Math.floor((brightness - 0.1) * 10) / 10
+                })`;
+                brightness = Math.floor((brightness - 0.1) * 10) / 10;
+                console.log("brightness", brightness);
+              }
             }
           });
           rowBoxes.push(rowBox);
